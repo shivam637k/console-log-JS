d81a901b89916784eb55ee774b2379982e8d8912
ShivamK <shivam637k@gmail.com>
2025-12-04 00:37:17 +0530

    operations in JS

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -6,7 +6,6 @@ let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);  // number
 // console.log((valueInNumber));  // NaN - Not a Number
 
-
 /*
     On conversion of variable 'score' -
         "33" => 33
@@ -23,8 +22,6 @@ let valueInNumber = Number(score)
 
 // --------------------------------------------------
 
-
-
 let isLoggedIn = 1;
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn)  // true
@@ -40,12 +37,54 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 
 
+
 // --------------------------------------------------
 
-
-
-
-let someNumber = 33
+let someNumber = true
 let stringSomeNumber = String(someNumber)
-console.log(stringSomeNumber)
-console.log(typeof stringSomeNumber)
+// console.log(stringSomeNumber)  // 33
+// console.log(typeof stringSomeNumber)  // string
+
+/*
+    On conversion of variable 'someNumber' -
+        null => null
+        undefined => undefined
+        33 => "33"
+        true => "true"
+        false => "false"
+*/
+
+
+
+
+
+
+
+// ********************************** OPERATIONS **********************************
+let val = 3
+let negVal = -val  // -ve value
+// console.log(negVal)
+
+// console.log(2 + 2);  // addition
+// console.log(2 - 2);  // subtraction
+// console.log(2 * 2);  // multiplication
+// console.log(2 / 2);  // division
+// console.log(2 % 2);  // modulo
+// console.log(2 ** 2);  // exponential
+
+
+let str1 = "hello"
+let str2 = " js"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2)  // 12
+// console.log(1 + "2")  // 12
+// console.log("1" + 2 + 2)  // 122
+// console.log(1 + 2 + "2")  // 32
+
+let gameCounter = 4
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
