2b38eb315cab687056f56c9417f2ebb8be55fb49
ShivamK <shivam637k@gmail.com>
2025-12-03 23:08:18 +0530

    data types in JS

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+/*
+    number = 2^53
+    bigint
+    string - ""
+    boolean - true/false
+    null - standalone value
+    undefined - value not assigned yet
+    symbol - uniqueness
+
+    object
+*/
+
+"use strict"; // treat JS code as newer version
+
+let name = "Alex"
+let age = 31
+let isLoggedIn = false
+let temp = null
+let state;
+
+console.log(typeof name);  // string
+console.log(typeof age);  // number
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+
+
